94be56bf4c3b686c1e614552e92f6c64437b3afc
Aleks B <aleksandir@me.com>
2023-12-03 00:32:15 +1030

    Refactor game count calculation logic

--- Day2/main..py
+++ Day2/main..py
@@ -1,6 +1,36 @@
+# Day2/input.txt
+
+with open("Day2/input.txt") as f:
+    lines = f.readlines()
+
+split_lines = []
+for line in lines:
+    line = line.replace(":", "").replace(",", "").replace(";", "")
+    split_lines.append(line.strip().split(" "))
+
 
 def main():
-    # Your code here
+    cubes = {"red": 12, "blue": 13, "green": 14}
+    # for every game/line
+    # for every cell in list
+    # ignoring the index 0 and 1, if cell in cubes
+    # compare the count of cubes in the dict with the count of cell -1 (left) of the cell
+    # if the count of the value pair in the dict is lower than the count in the game
+    # add the game number to the return value to sum up the games where the cube count is lower
+
+    return_value = 1
+
+    for game in split_lines:
+        index = 0
+        for cell in game[1:]:
+            if cell in cubes:
+                if cubes[cell] < int(game[index]):
+                    return_value += int(game[1].strip(":"))
+
+            index += 1
+
+    print(return_value)
+
 
 if __name__ == "__main__":
     main()
